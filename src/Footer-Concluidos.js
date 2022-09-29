289d5c8401f4488c51e4b0a99ef049ee9b50b26d
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-09-29 15:47:53 -0300

    criado o componente para o projeto

---
+++ src/Footer-Concluidos.js
@@ -0,0 +1,13 @@
+
+export default function FooterConcluidos (){
+  return(
+      <div className="footer-concluidos ">
+        <div className="container-botoes">
+        <button>dsd</button>
+        <button>dsd</button>
+        <button>dsd</button>
+      </div>
+          <p>concluido</p>
+      </div>
+  )
+}
